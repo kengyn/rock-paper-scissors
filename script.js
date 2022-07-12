206edfe7d0887e5122daae0cba161b29d866ba84
Ken <knetngyn@gmail.com>
2022-07-12 18:22:04 -0500

    Added safeguard for incorrect inputs

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let playerChoice = prompt('rock, paper, scissors?', '')
+// let playerChoice = prompt('rock, paper, scissors?', '')
 
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * 3)]
 }
 
-function playRound(player, computer) {
-    let playerSelec = player.toLowerCase()
-    let win = `wow sugoi winner winner ${player} beats ${computer}`
-    let lose = `lol rekt ${player} beats ${computer}`
-    if ((playerSelec == 'rock' && computer == 'scissors') || (playerSelec == 'scissors' && computer == 'paper') || (playerSelec == 'paper' && computer == 'rock'))  {
-        console.log(win)
-    } else if ((computer == 'rock' && playerSelec == 'scissors') || (computer == 'scissors' && playerSelec == 'paper') || (computer == 'paper' && playerSelec == 'rock')) {
-        console.log(lose)
+function playRound() {
+    let playerSelec = prompt('rock|paper|scissors').toLowerCase()
+    let computer = computerPlay()
+    let win = `wow sugoi winner winner ${playerSelec} beats ${computer}`
+    let lose = `lol rekt ${playerSelec} beats ${computer}`
+    if ((playerSelec == 'rock') || (playerSelec == 'scissors') || (playerSelec == 'paper')) {
+        if ((playerSelec == 'rock' && computer == 'scissors') || (playerSelec == 'scissors' && computer == 'paper') || (playerSelec == 'paper' && computer == 'rock'))  {
+            console.log(win)
+        } else if ((computer == 'rock' && playerSelec == 'scissors') || (computer == 'scissors' && playerSelec == 'paper') || (computer == 'paper' && playerSelec == 'rock')) {
+            console.log(lose)
+        } else {
+            console.log('tie')
+        }
     } else {
-        console.log('tie')
-    }
-    console.log(`player picked ${player} \n computer picked ${computer}`)
+        console.log(`you chose ${playerSelec}. pick rock, paper, or scissors`)
+        playRound()
+}
 }
