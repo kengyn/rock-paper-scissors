a93e3b43f62bd61198ef1cc07b72f44a0e700621
Ken <knetngyn@gmail.com>
2022-07-12 17:28:29 -0500

    Add computer player function

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 let playerChoice = prompt('rock, paper, scissors?', '')
+
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * 3)]
+}
