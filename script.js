fdbed46ff1fdbe21c89a83ae9df5bbf1390dda11
Ken <knetngyn@gmail.com>
2022-07-12 17:48:47 -0500

    Add playing function

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * 3)]
 }
+
+function playRound(player, computer) {
+    let playerSelec = player.toLowerCase()
+    let win = `wow sugoi winner winner ${player} beats ${computer}`
+    let lose = `lol rekt ${player} beats ${computer}`
+    if ((playerSelec == 'rock' && computer == 'scissors') || (playerSelec == 'scissors' && computer == 'paper') || (playerSelec == 'paper' && computer == 'rock'))  {
+        console.log(win)
+    } else if ((computer == 'rock' && playerSelec == 'scissors') || (computer == 'scissors' && playerSelec == 'paper') || (computer == 'paper' && playerSelec == 'rock')) {
+        console.log(lose)
+    } else {
+        console.log('tie')
+    }
+    console.log(`player picked ${player} \n computer picked ${computer}`)
+}
