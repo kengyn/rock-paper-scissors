9b11610bc3053c1540b9dfe3ecc433a58b0d35a9
Ken <knetngyn@gmail.com>
2022-07-12 19:09:59 -0500

    Remove safeguard and create game of 5 function

--- script.js
+++ script.js
@@ -8,18 +8,38 @@ function computerPlay() {
 function playRound() {
     let playerSelec = prompt('rock|paper|scissors').toLowerCase()
     let computer = computerPlay()
-    let win = `wow sugoi winner winner ${playerSelec} beats ${computer}`
-    let lose = `lol rekt ${playerSelec} beats ${computer}`
-    if ((playerSelec == 'rock') || (playerSelec == 'scissors') || (playerSelec == 'paper')) {
+    // if ((playerSelec == 'rock') || (playerSelec == 'scissors') || (playerSelec == 'paper')) {
         if ((playerSelec == 'rock' && computer == 'scissors') || (playerSelec == 'scissors' && computer == 'paper') || (playerSelec == 'paper' && computer == 'rock'))  {
-            console.log(win)
+            console.log(`wow sugoi winner winner ${playerSelec} beats ${computer}`)
+            return 'win'
         } else if ((computer == 'rock' && playerSelec == 'scissors') || (computer == 'scissors' && playerSelec == 'paper') || (computer == 'paper' && playerSelec == 'rock')) {
-            console.log(lose)
+            console.log(`lol rekt ${computer} beats ${playerSelec}`)
+            return 'lose'
         } else {
             console.log('tie')
+            return 'tie'
         }
-    } else {
-        console.log(`you chose ${playerSelec}. pick rock, paper, or scissors`)
-        playRound()
-}
+    }
+    
+//     else {
+//         console.log(`you chose ${playerSelec}. pick rock, paper, or scissors`)
+//         playRound()
+// }}
+
+function game() {
+    let playerwin = 0;
+    let computerwin = 0;
+    let tie = 0;
+    for (let i = 0; i < 5; i++) {
+        let result = playRound();
+        
+        if (result == 'win') {
+            playerwin++
+        } else if (result == 'lose'){
+            computerwin++
+        } else {
+            tie++
+        }
+        console.log(`player: ${playerwin}\ncomputer: ${computerwin}\ntie ${tie}`)
+    }
 }
