c5ae046b030ce263ec26f3ff35c8dbbbcc6ad5cd
Ken <knetngyn@gmail.com>
2022-07-14 21:07:57 -0500

    Add functions to announce winner after a player reaches 5 points

--- script.js
+++ script.js
@@ -31,48 +31,47 @@ function playRound(selection) {
     (playerSelec == "paper" && computer == "rock")
   ) {
     pScore.textContent += "x";
-    console.log(`wow sugoi winner winner ${playerSelec} beats ${computer}`);
-    return "win";
+    roundWinner(playerSelec, computer, "win");
+    game("win");
   } else if (
     (computer == "rock" && playerSelec == "scissors") ||
     (computer == "scissors" && playerSelec == "paper") ||
     (computer == "paper" && playerSelec == "rock")
   ) {
     cScore.textContent += "x";
-    console.log(`lol rekt ${computer} beats ${playerSelec}`);
-    return "lose";
+    roundWinner(playerSelec, computer, "lose");
+    game("lose");
   } else {
     tScore.textContent += "x";
-    console.log(`tie`);
-    return "tie";
+    roundWinner(playerSelec, computer, "tie");
+    game("tie");
   }
 }
 
-//function to loop 5 rounds of rock paper scissors and declare winner
-// function game() {
-//   let playerWin = 0;
-//   let computerWin = 0;
-//   let tie = 0;
-//   for (let i = 0; i < 5; i++) {
-//     let result = playRound();
-//     console.log(i);
-//     if (result == "win") {
-//       playerWin++;
-//     } else if (result == "lose") {
-//       computerWin++;
-//     } else {
-//       tie++;
-//     }
-//     console.log(`player: ${playerWin}\ncomputer: ${computerWin}\ntie ${tie}`);
-//   }
-//   if (playerWin > computerWin) {
-//     console.log("you win!!!");
-//   } else if (playerWin < computerWin) {
-//     console.log("you lose!!!");
-//   } else {
-//     console.log("tie!!!!");
-//   }
-// }
+// function to loop 5 rounds of rock paper scissors and declare winner
+let playerWin = 0;
+let computerWin = 0;
+let tie = 0;
+function game(result) {
+  if (result == "win") {
+    playerWin++;
+  } else if (result == "lose") {
+    computerWin++;
+  } else {
+    tie++;
+  }
+  checkWinner();
+}
+
+let checkWinner = () => {
+  if (playerWin == 5) {
+    alert("Winner: Player!!");
+  } else if (computerWin == 5) {
+    alert("Winner: Computer!!!");
+  } else if (tie == 5) {
+    alert("tie!!!!");
+  }
+};
 
 let rockbtn = document.querySelector(".rock");
 let paperbtn = document.querySelector(".paper");
@@ -83,6 +82,7 @@ let pScore = document.querySelector(".playerScore");
 let cScore = document.querySelector(".computerScore");
 let tScore = document.querySelector(".tie");
 let battleLog = document.querySelector(".log");
+const div = document.createElement("div");
 
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -94,4 +94,17 @@ function log(player, computer) {
   const p = document.createElement("p");
   p.textContent = `player:${player}\ncomputer:${computer}`;
   battleLog.appendChild(p);
+}
+
+function roundWinner(player, computer, result) {
+  if (result == "win") {
+    div.textContent = `wow sugoi winner winner ${player} beats ${computer}`;
+    score.appendChild(div);
+  } else if (result == "lose") {
+    div.textContent = `lol rekt ${computer} beats ${player}`;
+    score.appendChild(div);
+  } else if (result == "tie") {
+    div.textContent = "tie";
+    score.appendChild(div);
+  }
 }
